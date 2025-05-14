9ab6dfce067a73bce66b59b39f8341c1c30a60dc
Rishab Khivsara <rishabjain3500@gmail.com>
2025-05-15 01:35:41 +0530

    easier animation

--- app/(tabs)/two.tsx
+++ app/(tabs)/two.tsx
@@ -1,4 +1,4 @@
-import { StyleSheet, type LayoutChangeEvent } from "react-native";
+import { Pressable, StyleSheet, Text, type LayoutChangeEvent } from "react-native";
 
 import { View } from "@/components/Themed";
 import Step1 from "../../components/steps/Step1";
@@ -13,63 +13,93 @@ import Animated, {
   useAnimatedRef,
   FadeIn,
   FadeOut,
+  LinearTransition,
+  FadeInDown,
+  FadeOutDown,
+  FadeInUp,
 } from "react-native-reanimated";
+
+const _layout = LinearTransition.springify().damping(500).stiffness(500);
 
 export default function TabTwoScreen() {
   const [index, setIndex] = useState(0);
-  const height = useSharedValue<null | number>(null);
-  const stepRef = useAnimatedRef();
+  // const height = useSharedValue<null | number>(null);
+  // const stepRef = useAnimatedRef();
 
-  useEffect(() => {
-    runOnUI(() => {
-      const measured = measure(stepRef);
+  // useEffect(() => {
+  //   runOnUI(() => {
+  //     const measured = measure(stepRef);
 
-      console.log("🚀🚀🚀 ~ runOnUI ~ measured:", measured);
+  //     console.log("🚀🚀🚀 ~ runOnUI ~ measured:", measured);
 
-      if (measured) {
-        height.value = withTiming(measured.height, { duration: 300 });
-      }
-    })();
-  }, [index]);
+  //     if (measured) {
+  //       height.value = withTiming(measured.height, { duration: 300 });
+  //     }
+  //   })();
+  // }, [index]);
 
-  const animatedStyle = useAnimatedStyle(() => ({
-    height: height.value,
-  }));
+  // const animatedStyle = useAnimatedStyle(() => ({
+  //   height: height.value,
+  // }));
+
+  const [paddingTop, setPaddingTop] = useState(0);
+  const [showCalendarModal, setShowCalendarModal] = useState(false);
+
+  const toggleCalendarModal = () => setShowCalendarModal((p) => !p);
+  const hideCalendarModal = () => {
+    setShowCalendarModal(false);
+    setIndex(0);
+  };
 
   const onNext = () => setIndex((prev) => Math.min(prev + 1, 1));
   const onBack = () => setIndex((prev) => Math.max(prev - 1, 0));
 
   return (
-    <Animated.View style={styles.container}>
-      <Animated.View style={[styles.stepWrapper, animatedStyle]}>
+    <View style={styles.container}>
+      <Pressable
+        style={{
+          paddingHorizontal: 20,
+          paddingVertical: 10,
+          borderWidth: 1,
+          margin: 20,
+        }}
+        onPress={toggleCalendarModal}
+        onLayout={(e) => {
+          console.log(e.nativeEvent.layout, "onLayout");
+          setPaddingTop(e.nativeEvent.layout.y);
+        }}
+      >
+        <Text style={{ fontSize: 20 }}>Open Calendar</Text>
+      </Pressable>
+
+      <Animated.View
+        style={[styles.stepWrapper]}
+        entering={FadeInUp.duration(250)}
+        layout={_layout}
+      >
         <Animated.View
-          ref={stepRef}
-          entering={FadeIn}
-          exiting={FadeOut}
+          // ref={stepRef}
+          layout={_layout}
         >
           {index === 0 && <Step1 onNext={onNext} onBack={onBack} />}
           {index === 1 && <Step2 onNext={onNext} onBack={onBack} />}
         </Animated.View>
       </Animated.View>
-    </Animated.View>
+    </View>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    margin: 20,
-    gap: 20,
-    marginTop: "auto",
     justifyContent: "flex-end",
-    overflow: "hidden",
-    backgroundColor: "black",
+    backgroundColor: "green",
+    padding: 20,
   },
   stepWrapper: {
     backgroundColor: "white",
-    flex: 1,
     borderRadius: 20,
-    position: "absolute",
+    overflow: "hidden",
     width: "100%",
   },
 });
